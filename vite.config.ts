d73070e656a912a8112ff9c7915a2f9c58db2f50
ilcom <136066801+ilcom13@users.noreply.github.com>
2026-03-14 21:35:52 +0200

    Remove basic SSL plugin from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
-  base: '/',  // ← это главное исправление: корень сайта
+  plugins: [react()],
+  base: '/',
   build: {
-    outDir: './docs'  // оставляем, раз билд идёт в docs
+    outDir: './docs'
   }
-});
+})
